ae4a3b631af5d001dfa9136ae86a0cae1e1a67e9
petraartep <petra.pogacsas@gmail.com>
2019-06-13 12:51:07 +0100

    test helpers added

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -6,11 +6,7 @@ Capybara.app = Battle
 feature "Enter players" do  
 
   scenario "Players can fill in their names" do
-    visit("/")
-    fill_in :player_1, with: "Shaun" 
-    fill_in :player_2, with: "Theresa"
-    click_button "Submit"
-
+    sign_in_and_play
     expect(page).to have_content("Shaun vs. Theresa")
   end
 end
