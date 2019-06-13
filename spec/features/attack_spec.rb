0e2b2886c876492a65f215d3fe76406e4f206eeb
Danielandro <mtme13@hotmail.com>
2019-06-13 14:27:04 +0100

    Player 1 attacks Player 2

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,5 +1,7 @@
 feature "Attack player 2" do
   scenario "player 1 gets confirmation of attack" do
     sign_in_and_play    
+    click_link("Attack")     
+    expect(page).to have_content("Shaun attacks Theresa")
   end
 end
